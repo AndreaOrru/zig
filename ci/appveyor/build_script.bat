a7ea7146e1e0fac468a2cff371dd9ea41b87f82a
Andrew Kelley <superjoe30@gmail.com>
2017-05-23 10:05:09 -0400

    try again

--- ci/appveyor/build_script.bat
+++ ci/appveyor/build_script.bat
@@ -10,5 +10,6 @@ bash -lc "pacman -S --needed --noconfirm make mingw64/mingw-w64-x86_64-make ming
 bash -lc "ls /mingw64/lib"
 bash -lc "env"
 bash -lc "rm -rf '/c/Program Files/LLVM'"
+bash -lc "rm -rf '/c/Libraries/'"
 
 bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && mkdir build && cd build && cmake .. -G""MSYS Makefiles"" -DCMAKE_INSTALL_PREFIX=$(pwd) -DZIG_LIBC_LIB_DIR=$(dirname $(cc -print-file-name=crt1.o)) -DZIG_LIBC_INCLUDE_DIR=$(echo -n | cc -E -x c - -v 2>&1 | grep -B1 ""End of search list."" | head -n1 | cut -c 2- | sed ""s/ .*//"") -DZIG_LIBC_STATIC_LIB_DIR=$(dirname $(cc -print-file-name=crtbegin.o)) && make && make install"
